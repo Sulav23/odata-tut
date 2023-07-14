563f9051ff5f53154c559669718001a5cbc0e78e
Sulav <sulav,shrestha@xrdig.com>
2023-07-14 10:45:34 +0545

    initial commit

---
+++ src/formFields.ts
@@ -0,0 +1,14 @@
+export const formFields = [
+  {
+    field: "userId",
+    title: "UserId",
+  },
+  {
+    field: "id",
+    title: "Id",
+  },
+  {
+    field: "title",
+    title: "Title",
+  },
+];
